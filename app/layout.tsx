d974c61dd130647702f359153e517eff129ea390
ilike996 <49783687+ilike996@users.noreply.github.com>
2023-12-26 11:04:18 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Yiwen ChatGPT",
+  description: "ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
