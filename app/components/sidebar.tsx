08db54cc58d1bd08201a49e9a336160001a2b8ed
ilike996 <49783687+ilike996@users.noreply.github.com>
2023-12-26 12:21:54 +0800

    更新 sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          Yiwen ChatGPT
+          YiwenAI ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
            （内部测试 v0.3）
